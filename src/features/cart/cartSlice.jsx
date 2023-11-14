786b256ac4f4c47686acc26881d96842ec049405
Vinod Chandra <vinodchandra979@gmail.com>
2023-11-15 00:50:16 +0530

    add thunk api

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,12 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from '../../cartItems';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { openModal } from "../modal/modalSlice";
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 4,
     total: 0,
     isLoading : true,
 };
+
+export const getCartItems = createAsyncThunk('cart/getCartItem', async (name, thunkAPI)=>{
+    // try {
+    //     const resp = await fetch(url);
+    //     return await resp.json();
+    // } catch (err) {
+    //     return console.log();
+    // }
+
+
+    
+    try {
+        // console.log(name)
+        // console.log(name)
+        // console.log(thunkAPI)
+        // console.log(thunkAPI.getState())
+        // thunkAPI.dispatch(openModal())
+
+
+
+        const resp = await axios(url);
+        return resp.data;
+    } catch (err) {
+        return thunkAPI.rejectWithValue('something went wrong');
+    }
+})
 
 const cartSlice = createSlice({
     name: "cart",
@@ -37,7 +66,37 @@ const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
         }
-    }
+    },
+
+    // extraReducers:{
+    //     [getCartItems.pending]: (state)=>{
+    //         state.isLoading=true;
+    //     },
+    //     [getCartItems.fulfilled] : (state,action)=>{
+    //         console.log(action);
+    //         state.isLoading = false;
+    //         state.cartItems = action.payload;
+    //     },
+    //     [getCartItems.rejected]:(state, action)=>{
+    //         console.log(action)
+    //         state.isLoading = true;
+    //     },
+    // },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(getCartItems.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getCartItems.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.cartItems = action.payload;
+            })
+            .addCase(getCartItems.rejected, (state, action) => {
+                state.isLoading = true;
+            });
+    },
+
 })
 
 // console.log(cartSlice)
